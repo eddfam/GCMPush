dffd7280e86f17bc39a7dbcf4fe2bacafb3f5bf4
eddfam <air_boy_09@hotmail.com>
2015-11-10 18:04:32 -0600

    mainView F7

--- js/my-app.js
+++ js/my-app.js
@@ -5,10 +5,7 @@ var myApp = new Framework7();
 var $$ = Dom7;
 
 // Add view
-var mainView = myApp.addView('.view-main', {
-    // Because we use fixed-through navbar we can enable dynamic navbar
-    dynamicNavbar: true
-});
+
 
 
 /*
