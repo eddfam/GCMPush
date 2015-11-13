459089625cea70f8f1cd0b8d2a3fcea0913f6f59
eddfam <air_boy_09@hotmail.com>
2015-11-13 16:05:30 -0600

    page
    agregando paginas en el home y prueba con notificaciones

--- js/home.js
+++ js/home.js
@@ -3,6 +3,7 @@ var myApp = new Framework7({
     modalTitle: 'Framework7',
     // Enable Material theme
     material: true,
+    swipePanel: 'left',
 });
 
 // Expose Internal DOM library
@@ -10,7 +11,54 @@ var $$ = Dom7;
 
 // Add main view
 var mainView = myApp.addView('.view-main', {
+    domCache: true //enable inline pages
 });
+
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    onDeviceReady: function() {
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "400009158834"
+            },
+            "ios": {"alert": "true", "badge": "true", "sound": "true"}, 
+            "windows": {} 
+        });
+        
+        push.on('notification', function(data) {
+        	console.log("notification event");
+            console.log(JSON.stringify(data));
+            
+            if(data.title=='forms')
+                {
+                    mainView.router.load({pageName: 'forms'});
+                }
+            
+            push.finish(function () {
+                console.log('finish successfully called');
+            });
+        });
+
+        push.on('error', function(e) {
+            console.log("push error");
+            document.getElementById("regId").innerHTML = 'ERror';
+        });
+    }
+};
+
+
+app.initialize(); 
+
+
+
+
 // Add another view, which is in right panel
 var rightView = myApp.addView('.view-right', {
 });
