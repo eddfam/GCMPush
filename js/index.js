6f84a1fe6ae2576b8db4bb411adeb592b04b7843
eddfam <air_boy_09@hotmail.com>
2015-11-30 09:21:29 -0600

    &&

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var id = localStorage.getItem("regId");
 var nombreUsuario = localStorage.getItem("nombreUsuario");
 
-if(id === null & nombreUsuario===null)
+if(id === null && nombreUsuario===null)
     {
         console.log(id+nombreUsuario);
          window.location =("login.html");
