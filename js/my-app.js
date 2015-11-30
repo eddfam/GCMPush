e5e4c9b5dcda28f9850fafc377d0995a7b75a436
eddfam <air_boy_09@hotmail.com>
2015-11-30 15:35:51 -0600

    js

--- js/my-app.js
+++ js/my-app.js
@@ -16,7 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-  javascript:window.history.forward(1);
+
 
 var app = {
     // Application Constructor
@@ -83,3 +83,4 @@ var app = {
 
 app.initialize(); 
      
+  javascript:window.history.forward(1);
