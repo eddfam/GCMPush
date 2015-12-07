7210b70bf3d4f5c9411c39cbd4d2642658eadc04
eddfam <air_boy_09@hotmail.com>
2015-12-07 17:35:19 -0600

    offline

--- js/home.js
+++ js/home.js
@@ -20,15 +20,13 @@ var ptrContent = $$('.pull-to-refresh-content');
 // Add 'refresh' listener on it
 ptrContent.on('refresh', function (e) {
     setTimeout(function () {
-            
-        
         $.ajax({
                 url:'http://desde9.esy.es/noticias.php',
                 type:'POST',
                
                 dataType:'json',
                 error:function(jqXHR,text_status,strError){
-                    alert('no connection');
+                    alert('no internet connection');
                 }, 
                 timeout:60000,
                 success:function(data){
@@ -70,7 +68,7 @@ function startDB() {
         active = dataBase.result;
 
         object = active.createObjectStore("noticias", { keyPath : 'id', autoIncrement : true });
-        object.createIndex('by_idServer', 'idServer', { unique : true });
+        object.createIndex('by_serverId', 'serverId', { unique : true });
         object.createIndex('by_fecha', 'fecha', { unique : true });
     };
 
@@ -111,6 +109,60 @@ function add(datos) {
     };
 
 }
+}
+function cargarIDB(){
+    var active = dataBase.result;
+    var data = active.transaction(["noticias"], "readonly");
+    var object = data.objectStore("noticias");
+
+    var elements = [];
+
+    object.openCursor().onsuccess = function (e) {
+
+        var result = e.target.result;
+
+        if (result === null) {
+            return;
+        }
+
+        elements.push(result.value);
+        result.continue();
+
+    };
+
+    data.oncomplete = function () {
+
+        var outerHTML = '';
+
+        for (var key in elements) {
+
+            outerHTML += 
+                
+                
+                '<div class="card">'
+                            +'<div class="card-header">'+elements[key].titulo+'</div>'
+                            +'<div class="card-content">'
+                            +'<div class="card-content-inner">'+elements[key].descripcion+'</div>'
+                            +'</div>'
+                            +'<div class="card-footer">'+elements[key].fecha+'</div>'
+                        +'</div>'
+                /*'\n\
+            <tr>\n\
+                <td>' + elements[key].dni + '</td>\n\
+                <td>' + elements[key].name + '</td>\n\
+                <td>\n\
+                    <button type="button" onclick="load(' + elements[key].id + ')">Details</button>\n\
+                </td>\n\
+            </tr>'*/;
+
+        }
+
+        elements = [];
+        $("#result").html("");
+        document.querySelector("#result").innerHTML = outerHTML;
+    };
+
+
 }
 function clear() {
     var active = dataBase.result;
