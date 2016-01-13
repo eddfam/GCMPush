b573d5278311944b6a9abcf834f27a8473db9251
eddfam <air_boy_09@hotmail.com>
2016-01-13 13:41:15 -0600

    //

--- js/home.js
+++ js/home.js
@@ -9,7 +9,10 @@ var myApp = new Framework7({
 
 // Expose Internal DOM library
 var $$ = Dom7;
-
+$$('.open-right-panel').on('click', function (e) {
+        // 'right' position to open Right panel
+        myApp.openPanel('right');
+    });
 // Add main view
 var mainView = myApp.addView('.view-main',{
     domCache: true //enable inline pages
@@ -79,8 +82,8 @@ ptrNoticias.on('refresh', function (e) {
                 timeout:60000,
                 success:function(data){
                     $("#result").html("");
-                    clear();
-                    add(data);
+                    //clear();
+                    //add(data);
                     for(var i in data){
                     $("#result").append(
 
@@ -132,112 +135,112 @@ ptrPublicaciones.on('refresh', function (e) {
             });
             // When loading done, we need to "close" it
             myApp.pullToRefreshDone();
-        }, 2000);
+        }, 6000);
 });
       });
           
           
-          
-var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
-var dataBase = null;
-            
-
-function startDB() {
-            
-    dataBase = indexedDB.open("database", 1);
-
-    dataBase.onupgradeneeded = function (e) {
-
-        active = dataBase.result;
-
-        object = active.createObjectStore("noticias", { keyPath : 'id', autoIncrement : true });
-        object.createIndex('by_serverId', 'serverId', { unique : true });
-        object.createIndex('by_fecha', 'fecha', { unique : true });
-    };
-
-    dataBase.onsuccess = function (e) {
-        //alert('Base de datos cargada correctamente');
-
-    };
-
-    dataBase.onerror = function (e)  {
-        alert('Error cargando la base de datos');
-    };
-}
-function add(datos) {
-    var active = dataBase.result;
-    var data = active.transaction(["noticias"], "readwrite");
-    var object = data.objectStore("noticias");
-    
-    for(var i=0;i<datos.length;i++){
-        var request=object.put({ //Añadimos un nuevo registro en la DB
-            titulo:datos[i].titulo,
-            descripcion:datos[i].descripcion,
-            fecha:datos[i].fecha,
-            serverId:datos[i].id
-        });
-
-    request.onerror = function (e) {
-        alert(request.error.name + '\n\n' + request.error.message);
-    };
-
-    data.oncomplete = function (e) {
-        alert('Objeto agregado correctamente');
-    };
-
-}
-}
-function cargarIDB(){
-    var active = dataBase.result;
-    var data = active.transaction(["noticias"], "readonly");
-    var object = data.objectStore("noticias");
-
-    var elements = [];
-
-    object.openCursor().onsuccess = function (e) {
-
-        var result = e.target.result;
-
-        if (result === null) {
-            return;
-        }
-
-        elements.push(result.value);
-        result.continue();
-
-    };
-
-    data.oncomplete = function () {
-
-        var outerHTML = '';
-
-        for (var key in elements) {
-
-            outerHTML += 
-                
-                
-                '<div class="card">'
-                            +'<div class="card-header">'+elements[key].titulo+'</div>'
-                            +'<div class="card-content">'
-                            +'<div class="card-content-inner">'+elements[key].descripcion+'</div>'
-                            +'</div>'
-                            +'<div class="card-footer">'+elements[key].fecha+'</div>'
-                        +'</div>';
-
-        }
-
-        elements = [];
-        $("#result").html("");
-        document.querySelector("#result").innerHTML = outerHTML;
-    };
-
-
-}
-function clear() {
-    var active = dataBase.result;
-    var data = active.transaction(["noticias"], "readwrite");
-    var object = data.objectStore("noticias").clear();
-}
+//          
+//var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+//var dataBase = null;
+//            
+//
+//function startDB() {
+//            
+//    dataBase = indexedDB.open("database", 1);
+//
+//    dataBase.onupgradeneeded = function (e) {
+//
+//        active = dataBase.result;
+//
+//        object = active.createObjectStore("noticias", { keyPath : 'id', autoIncrement : true });
+//        object.createIndex('by_serverId', 'serverId', { unique : true });
+//        object.createIndex('by_fecha', 'fecha', { unique : true });
+//    };
+//
+//    dataBase.onsuccess = function (e) {
+//        //alert('Base de datos cargada correctamente');
+//
+//    };
+//
+//    dataBase.onerror = function (e)  {
+//        alert('Error cargando la base de datos');
+//    };
+//}
+//function add(datos) {
+//    var active = dataBase.result;
+//    var data = active.transaction(["noticias"], "readwrite");
+//    var object = data.objectStore("noticias");
+//    
+//    for(var i=0;i<datos.length;i++){
+//        var request=object.put({ //Añadimos un nuevo registro en la DB
+//            titulo:datos[i].titulo,
+//            descripcion:datos[i].descripcion,
+//            fecha:datos[i].fecha,
+//            serverId:datos[i].id
+//        });
+//
+//    request.onerror = function (e) {
+//        alert(request.error.name + '\n\n' + request.error.message);
+//    };
+//
+//    data.oncomplete = function (e) {
+//        alert('Objeto agregado correctamente');
+//    };
+//
+//}
+//}
+//function cargarIDB(){
+//    var active = dataBase.result;
+//    var data = active.transaction(["noticias"], "readonly");
+//    var object = data.objectStore("noticias");
+//
+//    var elements = [];
+//
+//    object.openCursor().onsuccess = function (e) {
+//
+//        var result = e.target.result;
+//
+//        if (result === null) {
+//            return;
+//        }
+//
+//        elements.push(result.value);
+//        result.continue();
+//
+//    };
+//
+//    data.oncomplete = function () {
+//
+//        var outerHTML = '';
+//
+//        for (var key in elements) {
+//
+//            outerHTML += 
+//                
+//                
+//                '<div class="card">'
+//                            +'<div class="card-header">'+elements[key].titulo+'</div>'
+//                            +'<div class="card-content">'
+//                            +'<div class="card-content-inner">'+elements[key].descripcion+'</div>'
+//                            +'</div>'
+//                            +'<div class="card-footer">'+elements[key].fecha+'</div>'
+//                        +'</div>';
+//
+//        }
+//
+//        elements = [];
+//        $("#result").html("");
+//        document.querySelector("#result").innerHTML = outerHTML;
+//    };
+//
+//
+//}
+//function clear() {
+//    var active = dataBase.result;
+//    var data = active.transaction(["noticias"], "readwrite");
+//    var object = data.objectStore("noticias").clear();
+//}
 
 
 var app = {
@@ -257,7 +260,7 @@ var app = {
             "windows": {} 
         });
         
-        push.on('notification', function(data) {
+        push.on('notification', function(data){
         	console.log("notification event");
             console.log(JSON.stringify(data));
             
@@ -268,7 +271,7 @@ var app = {
             else if(data.title=='Noticias')
                 {
                     mainView.router.load({pageName: 'noticias'});
-                    cargarIDB();
+                    //cargarIDB();
                 }
             else if(data.title=='Notificacion')
                 {
