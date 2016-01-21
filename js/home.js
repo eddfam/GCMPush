43240779b252465abf7ddbeb6fcc49aebe3bd078
eddfam <air_boy_09@hotmail.com>
2016-01-20 18:15:21 -0600

    map1

--- js/home.js
+++ js/home.js
@@ -34,7 +34,7 @@ var app = {
 app.initialize();
 $(document).ready(function(){
     var map;
-    $(document).ready(function(){
+   
       map = new GMaps({
         div: '#map',
         lat: 26.072427289759077, 
@@ -58,7 +58,7 @@ $(document).ready(function(){
           content: '<strong>Preparatoria Colón</strong><br>Jalapa No. 335, Rodríguez,<br>88630 Reynosa, Tamps.'
         }
       });
-    });
+  
     // Init App
     var myApp = new Framework7({
         modalTitle: 'Framework7',
