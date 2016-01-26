7ef4e1419a2eb500eb144bf4ebdad85625655b93
eddfam <air_boy_09@hotmail.com>
2016-01-26 17:26:03 -0600

    1

--- js/index.js
+++ js/index.js
@@ -1,42 +1,80 @@
-
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 var app = {
     // Application Constructor
-    initialize: function(){
+    initialize: function() {
         this.bindEvents();
     },
-    bindEvents: function(){
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    onDeviceReady: function(){
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
         var push = PushNotification.init({
-            "android": {"senderID": "400009158834"},
-            "ios": {"alert":"true", "badge":"true", "sound":"true"},
+            "android": {
+                "senderID": "1234567890"
+            },
+            "ios": {"alert": "true", "badge": "true", "sound": "true"}, 
             "windows": {} 
         });
+        
         push.on('registration', function(data) {
             console.log("registration event");
             document.getElementById("regId").innerHTML = data.registrationId;
             console.log(JSON.stringify(data));
         });
-        
-        push.on('notification', function(data){
-            (new Media('tone.wav')).play();
-            alert("desde");
-            console.log("notification event");
+
+        push.on('notification', function(data) {
+            alert("notification");
+        	console.log("notification event");
             console.log(JSON.stringify(data));
-            if(data.title=='Notificacion'){
-                myApp.openPanel('right');
-            }
-            push.finish(function(){
+            var cards = document.getElementById("cards");
+            var card = '<div class="row">' +
+		  		  '<div class="col s12 m6">' +
+				  '  <div class="card darken-1">' +
+				  '    <div class="card-content black-text">' +
+				  '      <span class="card-title black-text">' + data.title + '</span>' +
+				  '      <p>' + data.message + '</p>' +
+				  '    </div>' +
+				  '  </div>' +
+				  ' </div>' +
+				  '</div>';
+            cards.innerHTML += card;
+            
+            push.finish(function () {
                 console.log('finish successfully called');
             });
         });
-        push.on('error', function(e){
+
+        push.on('error', function(e) {
             console.log("push error");
-            document.getElementById("regId").innerHTML = 'Error';
         });
     }
 };
+
 app.initialize();
 
 $(document).ready(function(){
